935981efae12a14119a0fba11118b6a2410eba3c
mrneto23 <37846407+mrneto23@users.noreply.github.com>
2019-10-09 11:45:35 -0300

    Update BuscaEnderecoPorCep.py
    Removido código sem uso.

--- BuscaEnderecoPorCep.py
+++ BuscaEnderecoPorCep.py
@@ -5,23 +5,18 @@ import json
 class BuscaEndereco(object):
     """docstring for BuscaEndereco"""
 
+
     def __init__(self, cep):
         super(BuscaEndereco, self).__init__()
         self.cep = cep
         self.url = "http://viacep.com.br/ws/%s/json"
         self.endereco = json.load(urllib.request.urlopen(self.url % self.cep))
-        self.cep = self.endereco['cep']
-        self.logradouro = self.endereco['logradouro']
-        self.complemento = self.endereco['complemento']
-        self.bairro = self.endereco['bairro']
-        self.cidade = self.endereco['localidade']
-        self.uf = self.endereco['uf']
-        self.unidade = self.endereco['unidade']
-        self.ibge = self.endereco['ibge']
-        self.gia = self.endereco['gia']
+
 
     def __str__(self):
         return "%s" % (self.endereco)
 
+
     def getEndereco(self):
         return self.endereco
+
